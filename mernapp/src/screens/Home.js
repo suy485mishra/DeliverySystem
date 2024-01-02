8d436f927ef9eb34a9bfab3183a7e6cdf403d133
Suyash Mishra <121488134+suy485mishra@users.noreply.github.com>
2024-01-02 21:32:45 +0530

    heya

---
+++ mernapp/src/screens/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+function Home() {
+  return (
+   <>
+    <div><Navbar/></div>
+    <div>Body</div>
+    <div><Footer/> </div>
+   </> 
+  )
+}
+
+export default Home
